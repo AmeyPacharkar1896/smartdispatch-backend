2811876890cce4f77ce0ce14f3f7b4259b45d2cd
Devansh Mayatra <devanshm667@gmail.com>
2025-07-31 14:32:29 +0530

    made response changes to test controller

--- src/controllers/test.controller.js
+++ src/controllers/test.controller.js
@@ -1,19 +1,20 @@
 import { supabase } from '../config/db.js'
 import { asyncHandler } from '../utils/asyncHandler.js'
+import { ApiResponse } from '../utils/ApiResponse.js'
 
 export const testController = asyncHandler(async (req, res) => {
   const { data, error } = await supabase.from("users").select();
   console.log(data, error)
-  return res.status(200)
-    .json(
-      new ApiResponse(
-        200,
-        {
-          data: {
-            data, error
-          }
-        },
-        "OK"
+  const message = "fetched succesfully";
+  if (data) {
+    return res.status(200)
+      .json(
+        new ApiResponse(
+          200,
+          data,
+          message,
+          "OK"
+        )
       )
-    )
+  }
 })
